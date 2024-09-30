9c4f59d63a689c8e00fba3d4be04853e7e40af46
SreenandhMt <152959595+SreenandhMt@users.noreply.github.com>
2024-09-30 14:46:30 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:event_manager/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Book Event',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
